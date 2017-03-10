6de397e3d3152b7b650fbd76c3f94f4456a4990a
mras2303 <marek.kowalonek@gmail.com>
2017-03-10 21:19:35 +0100

    Init project of Hangman in Swift language

---
+++ Hangman/Hangman/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Hangman
+//
+//  Created by marek on 10.03.2017.
+//  Copyright © 2017 marek. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
