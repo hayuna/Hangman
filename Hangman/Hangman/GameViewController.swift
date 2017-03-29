6b1627c78f33946e625f1d44be0bb8f63e196c32
mras2303 <marek.kowalonek@gmail.com>
2017-03-29 22:31:24 +0200

    Add some function

--- Hangman/Hangman/GameViewController.swift
+++ Hangman/Hangman/GameViewController.swift
@@ -11,24 +11,65 @@ import Alamofire
 
 class GameViewController: UIViewController {
     
+    //group
     var selectedGroup : String = "";
+    //category
     var selectedCategory : String = "";
+    //answer
     var riddleWord : String = "";
-    var unknownLetter : [String]?;
+    //array with riddle
+    var unknownLetter : [String]?
+    //used letters
+    var usedLetters: [String] = []
+    //count of found letters
+    var foundCorrectLetter : Int = 0
     
     
-    
-    @IBOutlet weak var riddle: UILabel!
     @IBOutlet weak var used: UILabel!
     @IBOutlet weak var inputLetter: UITextField!
-    
     @IBOutlet weak var wordLabel: UILabel!
     
     @IBAction func checkLetter(_ sender: Any) {
-        
+        checkMyLetter(inputLetter.text!.uppercased())
     }
     
-    
+    func checkMyLetter(_ letter: String){
+        var checkedLetter: String
+        var countOfTypeLetter: Int = 0
+        var inputWordLength: Int = 0
+        
+        inputWordLength = letter.characters.count
+        inputLetter.text = ""
+        
+        //jesli wpisalem wiecej znakow niz 1 to sprawdza pierwszy
+        if(inputWordLength > 1){
+            checkedLetter = String(letter[letter.startIndex])
+        //jesli wpisalem mniej znakow niz 1 to nic nie rob
+        }else if(inputWordLength < 1){
+            return
+        }else{
+            checkedLetter = letter
+        }
+        
+        //jesli wpisana litera jest w tablicy uzytych liter - nic nie rob
+        if(usedLetters.contains(checkedLetter)){
+            return
+        //w przeciwnym wypadku dodaj wpisana litere do tablicy uzytych liter
+        }else{
+            usedLetters.append(checkedLetter)
+        }
+        
+        for i in 0..<riddleWord.characters.count{
+            let currentIndex = riddleWord.characters.index(riddleWord.startIndex, offsetBy: i)
+            //szukanie litery w zagadce
+            if(String(riddleWord.uppercased()[currentIndex]) == checkedLetter){
+                foundCorrectLetter += 1
+                unknownLetter![i] = checkedLetter
+                countOfTypeLetter += 1
+                
+            }
+        }
+    }
     
     override func viewDidLoad() {
         print(selectedGroup, selectedCategory)
@@ -57,10 +98,8 @@ class GameViewController: UIViewController {
     func startUnknownLetters(){
         unknownLetter = [String](repeating: "_", count: riddleWord.characters.count);
         for letter in unknownLetter! {
-            riddle.text?.append(letter+" ")
+            wordLabel.text?.append(letter+" ")
         }
-        
-        //
     }
     
 }
